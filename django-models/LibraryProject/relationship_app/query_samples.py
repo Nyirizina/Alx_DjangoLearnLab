f8eba8e03437122af15d639565950b925e6ade60
Manzi Ivan <manzi9ivan@gmail.com>
2026-01-16 15:51:32 +0200

    commit all

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,12 +3,15 @@ import django
 from relationship_app.models import Author, Book, Library, Librarian
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
-def query_book(muhizi):
-    author = Author.objects.get(name='Muhizi')
+def query_book(author_name):
+    author = Author.objects.get(name=author_name)
     return Book.objects.filter(author=author)
-def list_library(Ububiko):
-    library = Library.objects.get(name='Ububiko')
+def list_library(library_name):
+    library = Library.objects.get(name=library_name)
     return library.books.all()
-def retrieve_librarian(Ububiko):
-    library = Library.objects.get(name='Ububiko')
-    return library.librarian
+def retrieve_librarian(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
+
+if __name__ == "__main__":
+    pass
