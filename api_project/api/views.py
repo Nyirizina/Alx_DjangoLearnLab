55a6adfb6c49c96954a7442172a89e91235c6306
Manzi Ivan <manzi9ivan@gmail.com>
2026-02-08 23:33:12 +0200

    add all

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,3 +8,7 @@ from .serializers import BookSerializer
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
