fa3a2a2a59c2d6a24007b1911a84b51ffb7877ef
Manzi Ivan <manzi9ivan@gmail.com>
2026-02-13 02:01:15 +0200

    add apiviews to the code

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
 # API view for listing and creating authors
 # Create your views here.
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly]
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.filter(book = 'id')
